eb22c1681cdb040270871f561971a6ecfa114382
papis <ddiallo300@yahoo.com>
2022-01-07 20:38:28 +0100

    this is the initial commit

---
+++ social/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.author}'s post"
